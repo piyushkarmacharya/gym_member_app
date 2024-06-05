99b753c4f32028c0afc5ce1e3e79b00c98e2284d
piyushkarmacharya <Karmacharyapiyush65@gmail.com>
2024-06-05 22:16:59 +0545

    connected feedbackpage with api

--- lib/feedbackpage.dart
+++ lib/feedbackpage.dart
@@ -1,48 +1,189 @@
 import 'package:flutter/material.dart';
-
-class FeedbackPage extends StatefulWidget{
+import 'package:gym_member_app/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+class FeedbackPage extends StatefulWidget {
   const FeedbackPage({super.key});
-  State<FeedbackPage> createState()=>_FeedbackPageState();
-
+  State<FeedbackPage> createState() => _FeedbackPageState();
 }
-class _FeedbackPageState extends State<FeedbackPage>{
+
+class _FeedbackPageState extends State<FeedbackPage> {
+
+  Map message={};
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController ctr = TextEditingController();
+  Future<void> setFeedback() async {
+   
+    int mid=Provider.of<UserProvider>(context,listen: false).getMid();
+    String url = Provider.of<UserProvider>(context, listen: false).getUrl();
+    try {
+       final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+      
+      final res = await http.post(
+          Uri.parse("http://$url:8000/api/feedback"),
+          headers: {'content-Type': 'application/json'},
+          body: jsonEncode({'mid':mid,'feedback': ctr.text}));
+      if (res.statusCode == 200) {
+        setState(() {
+          message = jsonDecode(res.body);
+           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(0),
+                    bottomRight: Radius.circular(20),
+                    topLeft: Radius.circular(0),
+                    topRight: Radius.circular(20))),
+            backgroundColor: Colors.green,
+            margin: EdgeInsets.fromLTRB(
+                0,0.8 * screenHeight,0.1 * screenWidth,10),
+            behavior: SnackBarBehavior.floating,
+            duration: Duration(seconds: 1),
+            content: Center(child: Text(message['message'])),
+          ),);
+          Navigator.of(context).pop();
+    
+        });
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          duration: Duration(seconds: 1), content: Text("Connection problem")));
+    }
+  }
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                Navigator.of(context).pop();
-              },
-              icon: Icon(Icons.exit_to_app))
-        ],
-        toolbarHeight: 80,
-        centerTitle: true,
-        title: Row(
-          children: [
-            Container(
-              height: 60,
-              width: 80,
-              child: Image.asset(
-                "assets/images/logo.png",
-              ),
-            ),
-            Text(
-              "CLUB\nDESPERADO",
-              style: TextStyle(
-                  color: Color(0xFF1A1363),
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            )
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pop();
+                },
+                icon: Icon(Icons.exit_to_app))
           ],
+          toolbarHeight: 80,
+          centerTitle: true,
+          title: Row(
+            children: [
+              Container(
+                height: 60,
+                width: 80,
+                child: Image.asset(
+                  "assets/images/logo.png",
+                ),
+              ),
+              Text(
+                "CLUB\nDESPERADO",
+                style: TextStyle(
+                    color: Color(0xFF1A1363),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0,0,0,8),
+                child: Text("Please leave your feedback below : ",style: TextStyle(fontSize: 15),),
+              ),
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        maxLines: 10,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFF332F64),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFF332F64),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFF332F64),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        controller: ctr,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Feedback cannot be empty";
+                          }
+                          if (value.length > 500) {
+                            return "Feedback can only accept 500 letters";
+                          }
+                          if(value.length<4){
+                            return "Too short feedback";
+                          }
+                          if(RegExp(r'^\s*[^\s]{0,3}$').hasMatch(value)){
+                            return "invalid feedback";
+                          }
+                          
+                          return null;
+                        },
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if(_formKey.currentState!.validate()){
+                                  setFeedback();
+                                }else{
+                                  print("validate first");
+                                }
+                              },
+                              child: Text("Submit",style: TextStyle(color: Colors.white),),
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all<Color>(
+                                    const Color(0xFF1A1363)),
+                                shape: MaterialStateProperty.all<OutlinedBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(24)),
+                                ),
+                              ),
+                            ),
+                          ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ElevatedButton(style: ButtonStyle(
+                                    
+                                    shape: MaterialStateProperty.all<OutlinedBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(24)),
+                                    ),
+                                  ),onPressed: (){Navigator.of(context).pop();}, child: Text("Cancel")),
+                              )
+                        ],
+                      )
+                    ],
+                  )),
+            ],
+          ),
         ),
       ),
-      body: Text("Feedback page"),
-      
-    ),);
+    );
   }
 }
